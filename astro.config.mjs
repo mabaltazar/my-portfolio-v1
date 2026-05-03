1f3a7d19292734e4c28506a3d95a6072a6766e42
Mark Anthony Baltazar <baltazar.markanthony@gmail.com>
2026-05-03 22:17:46 +0800

    commit all

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: '#',
-  // base: '/',
+  // site: 'https://mabaltazar.github.io',
+  // base: '/my-portfolio-v1',
   vite: {
     plugins: [tailwindcss()]
   },
